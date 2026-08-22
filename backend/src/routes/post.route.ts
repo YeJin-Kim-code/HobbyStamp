adf215f771eea6c7db89fe74f7c00875cc743d0a
YeJin-Kim-code <109929675+YeJin-Kim-code@users.noreply.github.com>
2026-08-22 13:29:51 +0900

    Update post.route.ts

--- backend/src/routes/post.route.ts
+++ backend/src/routes/post.route.ts
@@ -5,5 +5,6 @@ import * as postController from "../controllers/post.controller";
 const router = Router();
 
 router.post("/", authenticate, postController.createPost);
-
+router.put("/:id", authenticate, postController.updatePost);
+router.delete("/:id", authenticate, postController.deletePost);
 export default router;
