ba0fb57aacab6035d4894ced13ad030c59c74966
YeJin-Kim-code <109929675+YeJin-Kim-code@users.noreply.github.com>
2026-08-27 10:25:53 +0900

    Update app.ts

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,19 +1,21 @@
-import express from 'express';
-import authRouter from './routes/auth.routes';
-import hobbyRoutes from "./routes/hobby.routes"
-import hobbyRecordRoutes from './routes/hobby-record.routes';
+import express from "express";
+import authRouter from "./routes/auth.routes";
+import hobbyRoutes from "./routes/hobby.routes";
+import hobbyRecordRoutes from "./routes/hobby-record.routes";
 import stampRoutes from "./routes/stamp.routes";
 import postRoutes from "./routes/post.route";
 import commentRoutes from "./routes/comment.routes";
+import dashboardRoutes from "./routes/dashboard.routes";
 const app = express();
 
 app.use(express.json());
 
-app.use('/auth', authRouter);
-app.use('/api/hobbies', hobbyRoutes);
-app.use('/api/hobby-records', hobbyRecordRoutes);
+app.use("/auth", authRouter);
+app.use("/api/hobbies", hobbyRoutes);
+app.use("/api/hobby-records", hobbyRecordRoutes);
 app.use("/stamps", stampRoutes);
 app.use("/posts", postRoutes);
-app.use(commentRoutes);
 
+app.use(commentRoutes);
+app.use("/dashboard", dashboardRoutes);
 export default app;
